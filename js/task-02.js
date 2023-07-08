b4db20f86614d6d9c2b2600ef4ad8a179811c909
hotgorod <hotgorod@gmail.com>
2023-07-08 10:48:39 +0100

    Task 02 done

--- js/task-02.js
+++ js/task-02.js
@@ -16,3 +16,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const elementsLi = ingredients.map(item => {
+  const li = document.createElement('li');
+  li.textContent = item;
+  li.classList.add('item')
+  return li
+});
+
+const listIngridients = document.querySelector('#ingredients')
+listIngridients.append(...elementsLi)
+console.log(listIngridients);
