af48c902b87278febfefe578b1b5abb42b9e9f57
hotgorod <hotgorod@gmail.com>
2023-07-08 17:41:01 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -9,3 +9,19 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+let counterValue = 0
+const buttonDec = document.querySelector('#counter button[data-action="decrement"]')
+
+const buttonInc = document.querySelector('#counter button[data-action="increment"]')
+
+const valueShown = document.querySelector('#value')
+
+buttonDec.addEventListener('click', () => {
+    counterValue -= 1;
+    valueShown.textContent = counterValue
+});
+buttonInc.addEventListener('click', () => {
+    counterValue += 1;
+    valueShown.textContent = counterValue
+});
